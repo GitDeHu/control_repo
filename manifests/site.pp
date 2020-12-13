b9f6942a5091f6aeef69629ea8a69373adf19b9d
GitDeHu <hubert@degonda.net>
2020-12-13 14:13:50 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,7 @@ node default {
     owner   => 'root',
   }
 }
+#spezifische node config. sobald dies für ein node existiert, wird default oben für diesen node nicht mehr angewendet!!
+node 'master.puppet.vm' {
+  include role::master_server
+}
