f54156a8b99984d22e0ac7104fc8428732712576
hubert <hubert@degonda.net>
2021-01-02 21:43:20 +0100

    change in agent_nodes

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,7 +1,17 @@
 # Class: profile::agent_nodes
 #
 class profile::agent_nodes {
+  #include dockeragent
+  #dockeragent::node {'web.puppet.vm':}
+  #dockeragent::node {'db.puppet.vm':}
+
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+
+dockeragent::node { "web.puppet.vm.${::fqdn}":
+  ports => ['10080:80'],
+}
+dockeragent::node { "db.puppet.vm.${::fqdn}":
+  ports => ['20080:80'],
+}
+
 }
