d7d967402d06d29e883bca1ce63cc57f6d5a1ed5
hubert <hubert@degonda.net>
2021-01-02 21:48:07 +0100

    agent1 und 2

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -7,10 +7,10 @@ class profile::agent_nodes {
 
   include dockeragent
 
-dockeragent::node { "web.puppet.vm.${::fqdn}":
+dockeragent::node { "agent1.${::fqdn}":
   ports => ['10080:80'],
 }
-dockeragent::node { "db.puppet.vm.${::fqdn}":
+dockeragent::node { "agent2.${::fqdn}":
   ports => ['20080:80'],
 }
 
